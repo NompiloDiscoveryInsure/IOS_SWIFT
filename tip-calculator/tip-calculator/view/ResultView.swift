a11cad2cfe724e4518c43d930f9f230fecf25bc8
NompiloDiscoveryInsure <nompilomo@discovery.co.za>
2023-11-13 08:39:55 +0200

    added amountView

--- tip-calculator/tip-calculator/view/ResultView.swift
+++ tip-calculator/tip-calculator/view/ResultView.swift
@@ -106,65 +106,6 @@ class ResultView: UIView{
         view.heightAnchor.constraint(equalToConstant: height).isActive = true
         return view
     }
-    
-    //7. create amountview class
-    class AmountView: UIView{
-        private let title: String
-        private let textAlignment: NSTextAlignment
-        
-        private lazy var titleLabel: UILabel = {
-            LabelFactory.build(
-                text: title,
-                font: Themefont.regular(ofSize: 18),
-                textColor: ThemeColor.text,
-                textAlignment: textAlignment)
-        }()
-        
-        
-        private lazy var AmoutLabel: UILabel = {
-            let label = UILabel()
-            label.textAlignment = textAlignment
-            label.textColor = ThemeColor.primary
-            let text = NSMutableAttributedString(
-                string: "$0",
-                attributes:
-                    [.font: Themefont.bold(ofSize: 24)])
-            text.addAttributes([.font: Themefont.bold(ofSize: 16)],
-                               range: NSMakeRange(0, 1))
-            label.attributedText = text
-            return label
-            
-        }()
-        
-        private lazy var stackView: UIStackView = {
-            let stackView = UIStackView(arrangedSubviews:[
-                titleLabel,
-                AmoutLabel
-            ])
-            stackView.axis = .vertical
-            return stackView
-        }()
-        
-        init(title: String, textAlignment: NSTextAlignment){
-            self.title = title
-            self.textAlignment = textAlignment
-            super.init(frame: .zero)
-            layout()
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        private func layout(){
-            addSubview(stackView)
-            stackView.snp.makeConstraints({
-                make in
-                make.edges.equalToSuperview()
-            })
-        }
-        
-    }
 }
 
 
